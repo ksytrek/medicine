8c7b25b9f7319955874b82981edc8496d8029ec8
SOMPHOL WILA <sompholwilaza101@gmail.com>
2022-02-25 17:17:47 +0700

    up

--- pharmacist/navbar.php
+++ pharmacist/navbar.php
@@ -101,6 +101,9 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="contact.html">ประวัติการขาย</a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html">ออกจากระบบ</a>
+                                </li>
                             </ul>
                         </div>
                     </nav>
